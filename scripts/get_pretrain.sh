addf836008d2b178c969d5cc0102c4141f8f7908
Ibrahim Dahiru Dahiru <34832399+Daheer@users.noreply.github.com>
2023-04-19 21:37:37 +0100

    Edited setup script

--- scripts/get_pretrain.sh
+++ scripts/get_pretrain.sh
@@ -1,16 +1,16 @@
 echo Downloading pretrained yolor_p6 weights...
-gdown "https://drive.google.com/uc?export=download&id=17tTY_7NdQgEdKT_zuOQAk53jidjo_b6r" -O pretrained_weights.pt
+gdown "https://drive.google.com/uc?export=download&id=17tTY_7NdQgEdKT_zuOQAk53jidjo_b6r" -O pretrained_weights.pt &>/dev/null
 cd yolor
 echo Installing repo requirements...
-pip install -qr requirements.txt
+pip install -qr requirements.txt &>/dev/null
 echo Setting up mish-cuda...
-git clone https://github.com/JunnYu/mish-cuda
+git clone https://github.com/JunnYu/mish-cuda &>/dev/null
 cd mish-cuda
 python setup.py build install &>/dev/null
 cd ..
 echo Setting up pytorch_wavelets...
-git clone https://github.com/fbcotter/pytorch_wavelets
+git clone https://github.com/fbcotter/pytorch_wavelets &>/dev/null
 cd pytorch_wavelets
-pip install .
+pip install . &>/dev/null
 cd ..
 echo Done!!
