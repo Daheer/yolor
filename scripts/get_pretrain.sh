ffb73e234c814b7517923b4dee91cce19a04465b
Ibrahim Dahiru Dahiru <34832399+Daheer@users.noreply.github.com>
2023-04-19 21:17:25 +0100

    Changed model preparation

--- scripts/get_pretrain.sh
+++ scripts/get_pretrain.sh
@@ -1,7 +1,12 @@
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1Tdn3yqpZ79X7R1Ql0zNlNScB1Dv9Fp76" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1Tdn3yqpZ79X7R1Ql0zNlNScB1Dv9Fp76" -o yolor_p6.pt
-rm ./cookie
-
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1UflcHlN5ERPdhahMivQYCbWWw7d2wY7U" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1UflcHlN5ERPdhahMivQYCbWWw7d2wY7U" -o yolor_w6.pt
-rm ./cookie
+gdown "https://drive.google.com/uc?export=download&id=17tTY_7NdQgEdKT_zuOQAk53jidjo_b6r" -O pretrained_weights.pt
+cd yolor
+pip install -qr requirements.txt
+git clone https://github.com/JunnYu/mish-cuda
+cd mish-cuda
+python setup.py build install &>/dev/null
+cd ..
+git clone https://github.com/fbcotter/pytorch_wavelets
+cd pytorch_wavelets
+pip install .
+cd ..
+bash scripts/get_pretrain.sh
